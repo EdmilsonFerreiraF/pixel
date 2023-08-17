0a239ec64f3fe474d646063ee59e44ce6a777201
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2023-08-17 17:27:49 -0300

    fix: ignore eslint

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{display:"none"}}
             src="https://www.facebook.com/tr?id=317000867449369&ev=PageView&noscript=1"
           />
         </noscript>
